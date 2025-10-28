b552d2db5487a2e57d341ed56604f2bfa037c766
xchinh <232745627+xchinh@users.noreply.github.com>
2025-10-29 02:25:40 +0700

    init project

---
+++ src/main/java/com/example/cnpmnc/CnpmncApplication.java
@@ -0,0 +1,19 @@
+package com.example.cnpmnc;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnpmncApplication {
+
+	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(
+                entry ->
+                    System.setProperty(entry.getKey(), entry.getValue())
+        );
+        SpringApplication.run(CnpmncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cnpmnc/CnpmncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cnpmnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnpmncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
