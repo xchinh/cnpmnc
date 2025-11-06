3c1debff5df7d8c5727a4ac249a966b15c2cdadd
TranDinhTuong <trandinhtuong088@gmail.com>
2025-11-06 23:25:17 +0700

    update logic

--- src/main/java/com/example/cnpmnc/config/SecurityConfig.java
+++ src/main/java/com/example/cnpmnc/config/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
                     .anyRequest().authenticated()
                 )
                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-               //.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())));
+               .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())));
        return httpSecurity.build();
    }
 
