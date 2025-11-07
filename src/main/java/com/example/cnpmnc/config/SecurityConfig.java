039c9d33df2d4103e340928176d7f8d05cf792b8
xchinh <232745627+xchinh@users.noreply.github.com>
2025-11-07 19:17:14 +0700

    deploy 6

--- src/main/java/com/example/cnpmnc/config/SecurityConfig.java
+++ src/main/java/com/example/cnpmnc/config/SecurityConfig.java
@@ -42,8 +42,9 @@
 
 package com.example.cnpmnc.config;
 
+import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -51,8 +52,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+//import org.springframework.security.oauth2.jwt.JwtDecoder;
+//import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
@@ -61,19 +62,21 @@ import org.springframework.web.filter.CorsFilter;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    @Value("${spring.security.oauth2.resource-server.jwt.secret}")
-    private String secretKey;
+//    @Value("${spring.security.oauth2.resource-server.jwt.secret}")
+//    private String secretKey;
 
     private final CorsFilter corsFilter; // Inject CorsFilter
+    private final Filter jwtVerifyFilter;
 
-   @Bean
+    @Bean
    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
        httpSecurity
                .csrf(AbstractHttpConfigurer::disable)
                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+               .addFilterBefore(jwtVerifyFilter, UsernamePasswordAuthenticationFilter.class)
                .authorizeHttpRequests(auth -> auth
                     .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/api/v1/auth/login", "/api/v1/auth/register").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/auth/login", "/auth/register").permitAll()
                     .requestMatchers(
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
@@ -82,17 +85,17 @@ public class SecurityConfig {
                     ).permitAll()
                     .anyRequest().authenticated()
                 )
-               .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-               .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())));
+               .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//               .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())));
        return httpSecurity.build();
    }
 
 
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withSecretKey(
-                new javax.crypto.spec.SecretKeySpec(secretKey.getBytes(), "HmacSHA256")
-        ).build();
-    }
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        return NimbusJwtDecoder.withSecretKey(
+//                new javax.crypto.spec.SecretKeySpec(secretKey.getBytes(), "HmacSHA256")
+//        ).build();
+//    }
 }
 
