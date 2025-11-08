ba216e9b64ababa51f0dd37a7453184f0ce85756
xchinh <232745627+xchinh@users.noreply.github.com>
2025-11-08 08:35:23 +0700

    done

--- src/main/java/com/example/cnpmnc/config/CorsConfig.java
+++ src/main/java/com/example/cnpmnc/config/CorsConfig.java
@@ -30,7 +30,7 @@ public class CorsConfig {
                 "http://127.0.0.1:5173",      // Vite với 127.0.0.1
                 "http://127.0.0.1:4200",       // Angular với 127.0.0.1
                 // Thêm production domain khi deploy
-                "https://cnpmnc.onrender.com"
+                "https://crm-fe-iota.vercel.app"
         ));
 
         // Cho phép tất cả headers
